338705eaceb280588fbf78d691715053a9f2faa7
Aahwan <vaahwan@gmail.com>
2023-02-01 17:56:00 +0530

    basket started

--- footer/footer.js
+++
@@ -1,9 +0,0 @@
-import { footer } from "./footercomponent.js";
-
-document.querySelector("footer").innerHTML = footer();
-
-// document.getElementById("aboutsephoraselector").addEventListener("click",showaboutsephora)
-
-// function showaboutsephora(){
-//     document.getElementById("aboutsephora").style.display = "block";
-// }
